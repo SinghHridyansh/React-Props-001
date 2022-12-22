1ddb33db434609080ea28aa3558f6238fd1e4c12
SinghHridyansh <Hridyansh.360@gmail.com>
2022-12-22 20:58:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
 function App() {
+  const Tab = (props) =>{
+    return(
+      <div className="Tab">
+        <div className="box" style={{backgroundColor:props.bgcolor}}>
+        </div>
+        <div className="content">
+          <h2>{props.heading}</h2>
+          <p style={{color:props.bgcolor}}>{props.code}</p>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Tab heading="#FF6663" code="PINK" bgcolor='#FF6663'></Tab>
+      <Tab heading="#333333" code="GRAY" bgcolor='#333333'></Tab>
+      <Tab heading="#000000" code="BLACK" bgcolor='#000000'></Tab>
+      <Tab heading="#38BB14" code="GREEN" bgcolor='#38BB14'></Tab>
+      <Tab heading="#C90B0B" code="RED" bgcolor='#C90B0B'></Tab>
+      <Tab heading="#FF8000" code="ORANGE" bgcolor='#FF8000'></Tab>
+      <Tab heading="#FFF500" code="YELLOW" bgcolor='#FFF500'></Tab>
+      <Tab heading="#CCCCCC" code="LIGHT GRAY" bgcolor='#CCCCCC'></Tab>
+      <Tab heading="#7E41A2" code="PURPLR" bgcolor='#7E41A2'></Tab>
+      <Tab heading="#C14911" code="bROWN" bgcolor='#C14911'></Tab>
+      
     </div>
   );
 }
